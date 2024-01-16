e156226d6923e36b51e8dc12f2a09b2334f28978
Dominicus0830 <100403377+Dominicus0830@users.noreply.github.com>
2024-01-16 13:46:24 +0900

    InventoryUtils update

--- src/main/java/org/CatAndDomi/utils/InventoryUtils.java
+++ src/main/java/org/CatAndDomi/utils/InventoryUtils.java
@@ -7,7 +7,7 @@ import org.bukkit.inventory.ItemStack;
 import java.util.HashMap;
 
 public class InventoryUtils {
-    public static boolean hasEnoughSpace(ItemStack[] content, ItemStack item) {
+    public static boolean hasEnoughSpace(ItemStack[] content, ItemStack... item) {
         Inventory inv = Bukkit.createInventory(null, 36);
         inv.setContents(content);
         HashMap<Integer, ItemStack> leftover = new HashMap<>();
