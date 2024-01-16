d23fe48c2dc9308f87b5d6458be68946490ca8a7
Dominicus0830 <100403377+Dominicus0830@users.noreply.github.com>
2024-01-16 15:20:02 +0900

    UpDate InventoryAPI

--- src/main/java/org/CatAndDomi/api/InventoryAPI.java
+++ src/main/java/org/CatAndDomi/api/InventoryAPI.java
@@ -1,4 +1,18 @@
 package org.CatAndDomi.api;
 
-public class InventoryAPI extends Craft{
+import org.bukkit.craftbukkit.v1_20_R3.inventory.CraftInventoryCustom;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InventoryAPI extends CraftInventoryCustom {
+
+
+    public InventoryAPI(InventoryHolder holder, String title, int size, JavaPlugin plugin) {
+        super(holder, size, title);
+    }
+
+    public InventoryAPI(InventoryHolder holder, String title, int size, boolean usePage, JavaPlugin plugin) {
+        super(holder, size, title);
+    }
 }
