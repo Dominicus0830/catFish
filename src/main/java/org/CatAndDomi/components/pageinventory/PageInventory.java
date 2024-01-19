169a1ad67036eee32a05c2f312315243bbb870b5
BuildTools <unconfigured@null.spigotmc.org>
2024-01-19 22:18:29 +0900

    PAGEINVENTORY 컴포넌트 추가

---
+++ src/main/java/org/CatAndDomi/components/pageinventory/PageInventory.java
@@ -0,0 +1,123 @@
+package org.CatAndDomi.components.pageinventory;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PageInventory {
+
+    public final String name;
+    public int size = 0;
+    public final int invsize;
+    public final Map<Integer, Inventory> invmap = new HashMap<>();
+    final PageInventoryComponent component;
+
+    public PageInventory(String name, PageInventoryComponent component, Integer invsize) {
+        this.name = name;
+        this.component = component;
+        this.invsize = invsize;
+    }
+
+    public PageInventory(PageInventoryComponent component, YamlConfiguration config) {
+        this.component = component;
+        this.name = config.getString("name");
+        this.size = config.getInt("size");
+        this.invsize = config.getInt("invsize");
+        for(int i = 0; i<size; i++) {
+            addpage();
+            Inventory inv = invmap.get(i);
+            for(int j = 0; j<invsize; j++) {
+                if(config.isSet(i+"."+j)) {
+                    inv.setItem(j, config.getItemStack(i+"."+j));
+                }
+            }
+        }
+    }
+
+    public void save(YamlConfiguration config) {
+        config.set("name", name);
+        config.set("size", size);
+        config.set("invsize", invsize);
+        for(int i = 0; i<size; i++) {
+            Inventory inv = invmap.get(i);
+            for(int j = 0; j<invsize; j++) {
+                config.set(i+"."+j, inv.getItem(j));
+            }
+        }
+    }
+
+    public void addpage() {
+        Inventory inv = Bukkit.createInventory(null, invsize);
+        setinv_creating(inv);
+        invmap.put(size, inv);
+        size++;
+    }
+
+    public void createpage(int page) {
+        if(page>-1) {
+            while(size<page) {
+                addpage();
+            }
+        }
+    }
+
+    public String InventoryName() {
+        return name;
+    }
+
+    public boolean openInventory(Player p, int page) {
+        if(page>=size) {
+            return false;
+        }
+        p.closeInventory();
+        Inventory inv = Bukkit.createInventory(null, invsize, InventoryName());
+        Inventory in = invmap.get(page);
+        for(int i = 0; i<invsize; i++) {
+            ItemStack a = in.getItem(i).clone();
+            setitem_opening(a, p, page);
+            inv.setItem(i, a);
+        }
+        setinv_opening(inv, p, page);
+        p.openInventory(inv);
+        component.openermap.put(p, new InventoryOpener(this, page));
+        return true;
+    }
+
+    public void setItem(int page, int slot, ItemStack i) {
+        ItemStack ia = i.clone();
+        setitem_setting(ia);
+        invmap.get(page).setItem(slot, ia);
+    }
+
+    public void setitem_setting(ItemStack i) {
+
+    }
+
+    public void setitem_opening(ItemStack i, Player p, int page) {
+
+    }
+
+    public void setinv_opening(Inventory inv, Player p, int page) {
+
+    }
+
+    public void setinv_creating(Inventory inv) {
+
+    }
+
+    public void close(InventoryCloseEvent e) {
+
+    }
+
+    public void click(InventoryClickEvent e) {
+
+    }
+
+}
---
+++ src/main/java/org/CatAndDomi/components/pageinventory/PageInventoryComponent.java
@@ -0,0 +1,94 @@
+package org.CatAndDomi.components.pageinventory;
+
+import org.CatAndDomi.components.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PageInventoryComponent extends Component {
+
+    Map<String, PageInventory> invmap = new HashMap<>();
+    Map<Player, InventoryOpener> openermap = new HashMap<>();
+
+    Class<? extends PageInventory> clz;
+    Class<? extends InventoryOpener> clz1;
+
+    public PageInventoryComponent(JavaPlugin plugin) {
+        super(plugin);
+        Bukkit.getPluginManager().registerEvents(new Events(this), plugin);
+        clz = PageInventory.class;
+        clz1 = InventoryOpener.class;
+        File file = new File(plugin.getDataFolder()+"/inventorylist.yml");
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        for(String key : config.getKeys(true)) {
+            File file1 = new File(plugin.getDataFolder()+"/inventorydata/" + key + ".yml");
+            YamlConfiguration config1 = YamlConfiguration.loadConfiguration(file);
+            PageInventory inv = new PageInventory(this, config1);
+            invmap.put(key, inv);
+        }
+    }
+
+    public void save() {
+        File file = new File(plugin.getDataFolder()+"/inventorylist.yml");
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(file);
+        for(Map.Entry<String, PageInventory> entry : invmap.entrySet()) {
+            File file1 = new File(plugin.getDataFolder()+"/inventorydata/" + entry.getKey() + ".yml");
+            YamlConfiguration config1 = YamlConfiguration.loadConfiguration(file);
+            config.set(entry.getKey(), true);
+            entry.getValue().save(config1);
+            try{
+                config1.save(file1);
+            }catch(Exception e) {
+            }
+        }
+        try{
+            config.save(file);
+        }catch(Exception e) {
+        }
+    }
+
+    public void setPageInventoryClass(Class<? extends PageInventory> clz) {
+        this.clz = clz;
+    }
+
+    public void setInventoryOpenerClass(Class<? extends InventoryOpener> clz1) {
+        this.clz1 = clz1;
+    }
+
+    public boolean createInventory(String name, int invsize) {
+        if(invmap.get(name)!=null) {
+            return false;
+        }
+        try{
+            invmap.put(name, clz.getDeclaredConstructor(new Class[]{String.class, PageInventoryComponent.class, Integer.class}).newInstance(name, this, invsize));
+            return true;
+        }catch(Exception e) {
+            return false;
+        }
+    }
+
+    public boolean removeInventory(String name) {
+        if(invmap.get(name)!=null) {
+            invmap.remove(name);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isInventory(String name) {
+        return invmap.get(name)!=null;
+    }
+
+    public PageInventory getInventory(String name) {
+        if(invmap.get(name)!=null) {
+            return invmap.get(name);
+        }
+        return null;
+    }
+}
